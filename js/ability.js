a8f79f059192d0de389ceb3e28014ab449f51d42
Nimscore <gamenimsi@gmail.com>
2022-09-27 11:50:06 +0300

    up

--- js/ability.js
+++ js/ability.js
@@ -8,18 +8,6 @@ $(document).ready(function() {
 	});  
 	// скролл вверх выбранного итема меню
 	
-	// аккордеон начало
-	$(".accordion").click(function(){
-		if($(".panel").css('display') == 'none')
-		{
-			$(".panel").slideDown(200);
-		}
-		else {
-			$(".panel").slideUp(200);
-		}
-	});  
-	// аккордеон конец
-	
 	
 	$(".mobHeaderDots").click(function(){
 		
@@ -104,92 +92,6 @@ $(document).ready(function() {
 		$(".container").removeClass("fixed");
 	
 	});
-	
-	
-	
-	
-	
-	// анимация попапа
-	// $(".mobHeaderDots").click(function(){
-	// 		$(".popup").animate({
-	// 			width: "toggle"
-	// 	}, 150);
-	// });
-	// анимация попапа
-	
-	// работа попапа
-	// $(".mobHeaderDots").click(function(){
-	// 		$(".container").toggleClass("fixed");
-	// });
-	
-	
-	// $(".mobHeaderDots").click(function(){
-	// 	if($(".popup").css('display') == 'block')
-	// 	{
-	// 		$(".container").addClass("fixed");
-	// 	}
-	// 	else {
-	// 		$(".container").removeClass("fixed");
-	// 	}
-	// });
-	
-	
-	
-	// работа попапа
-	
-	// трай свапа хэдера
-	// $(".mobHeaderDots").click(function(){
-	// 	if($("#header-mob").attr('display') == 'block')
-	// 	{
-	// 		$("#popup-header-mob").attr('display', 'block');
-	// 		$("#header-mob").attr('display', 'none');
-	// 	}
-	// 	else {
-	// 		$("#header-mob").attr('display', 'block');
-	// 		$("#popup-header-mob").attr('display', 'none');
-	// 	}
-	// });
-	
-	// $(".mobHeaderDots").click(function(){
-	// 	if($(".popup").css('display') == 'block')
-	// 	{
-	// 		$("#popup-header-mob").css("display", "block");
-	// 		$("#header-mob").css("display", "none");
-	// 	}
-	// 	else if ($(".popup").css('display') == 'none') {
-	// 		$("#header-mob").css("display", "block");
-	// 		$("#popup-header-mob").css("display", "none");
-	// 	}
-	// });
-	
-	
-	
-	
-	
-	
-	// трай свапа хэдера
-	
-	// скрытие попапа по попапу
-	// $(".popup").click(function(){
-	// 	$(".popup").css("display", "none");
-	// });
-	// скрытие попапа по попапу
-	
-	// $(".mobHeaderDots").click(function(){
-	// 	if($(".popup").css('display') == 'none')
-	// 	{
-	// 		$(".container").css("position", "auto");
-	// 		$(".popup").animate({
-	// 			width: "toggle"
-	// 	}, 150);
-	// 	}
-	// 	else {
-	// 		$(".container").css("position", "fixed");
-	// 		$(".popup").animate({
-	// 			width: "toggle"
-	// 	}, 150);
-	// 	}
-	// });
 	
 	});
 	
