07a243c5185a4644b88e192dbebc706adf6b7e34
Nimscore <gamenimsi@gmail.com>
2022-09-28 13:00:19 +0300

    up

--- js/gameplay.js
+++ js/gameplay.js
@@ -97,11 +97,19 @@ $(document).ready(function() {
 	$(".btn-l").click(function(){
 			$(".tab-l").fadeIn (600);
 			$(".tab-r").fadeOut (0);
+			// $(".tab-l").attr('color', '#00ff99');
+			// $(".tab-r").attr('color', '#9d9063');
+			$(".btn-l").addClass("select");
+			$(".btn-r").removeClass("select");
 	});
 
 	$(".btn-r").click(function(){
 			$(".tab-r").fadeIn (600);
 			$(".tab-l").fadeOut (0);
+			// $(".tab-r").attr('color', '#00ff99');
+			// $(".tab-l").attr('color', '#9d9063');
+			$(".btn-r").addClass("select");
+			$(".btn-l").removeClass("select");
 	});
 
 
