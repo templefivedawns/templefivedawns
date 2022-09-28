c6b1c080a15387d24afa048d29bc3b7bba7ec336
Nimscore <gamenimsi@gmail.com>
2022-09-28 13:51:38 +0300

    up

---
+++ js/talents.js
@@ -0,0 +1,117 @@
+$(document).ready(function() {
+
+	// скролл вверх выбранного итема меню
+	$(".select").click(function(){
+		$("html,body").animate({
+			scrollTop: 0
+	}, 200);
+	});  
+	// скролл вверх выбранного итема меню	
+	
+	$(".mobHeaderDots").click(function(){
+		
+		$(".mobHeaderDots").css("display", "none");
+		$(".mobHeaderDotsHide").css("display", "block");
+	
+	});
+	
+	$(".mobHeaderDotsHide").click(function(){
+		
+		$(".mobHeaderDots").css("display", "block");
+		$(".mobHeaderDotsHide").css("display", "none");
+	
+	});
+	
+	
+	$(".mobHeaderDots").click(function(){
+		$(".popup").animate({
+			width: "70vw"
+	}, 150);
+	
+	$(".popupBackground").animate({
+		width: "100%"
+	}, 150);
+	
+	
+	$(".popup-menu").fadeIn(150);
+	
+	$("#header-mob").css("display", "none");
+	$("#popup-header-mob").css("display", "block");
+	
+	});
+	
+	
+	$(".mobHeaderDotsHide").click(function(){
+		$(".popup").animate({
+			width: "0%"
+	}, 150);
+	
+	$(".popupBackground").animate({
+		width: "0%"
+	}, 150);
+	
+	
+	$(".popup-menu").fadeOut(150);
+	
+	$("#header-mob").css("display", "block");
+	$("#popup-header-mob").css("display", "none");
+	
+	});
+	
+	
+	$(".popupBackground").click(function(){
+		$(".popup").animate({
+			width: "0%"
+	}, 150);
+	
+	$(".popupBackground").animate({
+		width: "0%"
+	}, 150);
+	
+	
+	$(".popup-menu").fadeOut(150);
+	
+	$(".mobHeaderDots").css("display", "block");
+		$(".mobHeaderDotsHide").css("display", "none");
+		$(".container").removeClass("fixed");
+	
+		$("#header-mob").css("display", "block");
+	$("#popup-header-mob").css("display", "none");
+	
+	});
+	
+	$(".mobHeaderDots").click(function(){
+		
+		$(".container").addClass("fixed");
+	
+	});
+	
+	$(".mobHeaderDotsHide").click(function(){
+		
+		$(".container").removeClass("fixed");
+	
+	});
+
+
+
+	$(".btn-l").click(function(){
+			$(".tab-l").fadeIn (600);
+			$(".tab-r").fadeOut (0);
+			// $(".tab-l").attr('color', '#00ff99');
+			// $(".tab-r").attr('color', '#9d9063');
+			$(".btn-l").addClass("select");
+			$(".btn-r").removeClass("select");
+	});
+
+	$(".btn-r").click(function(){
+			$(".tab-r").fadeIn (600);
+			$(".tab-l").fadeOut (0);
+			// $(".tab-r").attr('color', '#00ff99');
+			// $(".tab-l").attr('color', '#9d9063');
+			$(".btn-r").addClass("select");
+			$(".btn-l").removeClass("select");
+	});
+
+
+
+});
