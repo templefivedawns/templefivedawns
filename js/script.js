57e343b2b08f25032f13f543bdb1c332fee8c0b1
Nimscore <gamenimsi@gmail.com>
2022-09-29 23:13:14 +0300

    hh

--- js/script.js
+++ js/script.js
@@ -1055,7 +1055,97 @@ $(".firstIII").mouseleave(function (){
 
 
 
+$(".mobHeaderDots").click(function(){
+	
+	$(".mobHeaderDots").css("display", "none");
+	$(".mobHeaderDotsHide").css("display", "block");
+	$(".popup-header-mob-item").css("display", "block");
+	$(".discordMob").css("display", "none");
+	$(".boostyMob").css("display", "none");
+	$(".containerHMob p").css("display", "none");
 
+});
+
+$(".mobHeaderDotsHide").click(function(){
+	
+	$(".mobHeaderDots").css("display", "block");
+	$(".mobHeaderDotsHide").css("display", "none");
+	$(".popup-header-mob-item").css("display", "none");
+	$(".discordMob").css("display", "block");
+	$(".boostyMob").css("display", "block");
+	$(".containerHMob p").css("display", "block");
+
+});
+
+
+$(".mobHeaderDots").click(function(){
+	$(".popup").animate({
+		width: "70vw"
+}, 150);
+
+$(".popupBackground").animate({
+	width: "100%"
+}, 150);
+
+
+$(".popup-menu").fadeIn(150);
+
+$("#header-mob").css("display", "none");
+$("#popup-header-mob").css("display", "block");
+
+});
+
+
+$(".mobHeaderDotsHide").click(function(){
+	$(".popup").animate({
+		width: "0%"
+}, 150);
+
+$(".popupBackground").animate({
+	width: "0%"
+}, 150);
+
+
+$(".popup-menu").fadeOut(150);
+
+$("#header-mob").css("display", "block");
+$("#popup-header-mob").css("display", "none");
+
+});
+
+
+$(".popupBackground").click(function(){
+	$(".popup").animate({
+		width: "0%"
+}, 150);
+
+$(".popupBackground").animate({
+	width: "0%"
+}, 150);
+
+
+$(".popup-menu").fadeOut(150);
+
+$(".mobHeaderDots").css("display", "block");
+	$(".mobHeaderDotsHide").css("display", "none");
+	$(".container").removeClass("fixed");
+
+	$("#header-mob").css("display", "block");
+$("#popup-header-mob").css("display", "none");
+
+});
+
+$(".mobHeaderDots").click(function(){
+	
+	$(".container").addClass("fixed");
+
+});
+
+$(".mobHeaderDotsHide").click(function(){
+	
+	$(".container").removeClass("fixed");
+
+});
 
 
 
