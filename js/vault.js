dc785e8d9df3aedb02d2d08c227a7a36ead19834
Nimscore <gamenimsi@gmail.com>
2022-09-30 13:41:13 +0300

    up

--- js/vault.js
+++ js/vault.js
@@ -93,5 +93,48 @@ $(document).ready(function() {
 	
 	});
 	
+// ХЭДЕР МЕНЮ БРАШ
+
+$(".WW").hover(function(){
+	$(".WWbrush").fadeIn();
+
+
+});
+
+$(".WW").mouseleave(function(){
+	$(".WWbrush").fadeOut();
+
+});
+
+
+
+
+$(".MW").hover(function(){
+	$(".MWbrush").fadeIn();
+
+
+});
+
+$(".MW").mouseleave(function(){
+	$(".MWbrush").fadeOut();
+
+});
+
+
+
+
+$(".Support").hover(function(){
+	$(".Supportbrush").fadeIn();
+
+
+});
+
+$(".Support").mouseleave(function(){
+	$(".Supportbrush").fadeOut();
+
+});
+
+// ХЭДЕР МЕНЮ БРАШ
+
 	});
 	
