f5f188129680ef76216a4ec8aa963c6d6f302d47
Nimscore <gamenimsi@gmail.com>
2022-10-28 16:11:48 +0300

    Update talents.js

--- js/monk/talents.js
+++ js/monk/talents.js
@@ -66,16 +66,7 @@ $(document).ready(function () {
 	});
 // СВАП
 
-	// let tt1 = document.querySelector('#tt1');
-	// $(window).resize(function() {
-	// 	if ($(window).width() < 480) {
-	// 		tt1.setAttribute("data-disable-wowhead-tooltip", "true")
-	// 	} else {
-	// 		tt1.setAttribute("data-disable-wowhead-tooltip", "false")
-	// 	}
-	// });
-
-	// 	ТАЛАНТЫ ХМЕЛЕВАРА 1-4
+	// 	ТАЛАНТЫ ХМЕЛЕВАРА 1-5
 	$("#tt1").click(function () {
 		let name1 = "keg smash"
 		let name2 = $("#tn1").text()
@@ -83,7 +74,6 @@ $(document).ready(function () {
 			$(".row1-tc").css("display", "none");
 			$("#tn1").text("none")
 		} else {
-			$(".row2-tc").css("display", "none");
 			$(".row1-tc").fadeIn (600);
 			$("#tn1").text("keg smash");
 			$("#tc1").text("Lorem ipsum dolor sit amet, onsectetur adipiscing elit. " +
@@ -99,7 +89,6 @@ $(document).ready(function () {
 			$(".row1-tc").css("display", "none");
 			$("#tn1").text("none")
 		} else {
-			$(".row2-tc").css("display", "none");
 			$(".row1-tc").fadeIn (600);
 			$("#tn1").text("stagger");
 			$("#tc1").text("Lorem ipsum dolor sit amet, onsectetur adipiscing elit. " +
@@ -115,7 +104,6 @@ $(document).ready(function () {
 			$(".row1-tc").css("display", "none");
 			$("#tn1").text("none")
 		} else {
-			$(".row2-tc").css("display", "none");
 			$(".row1-tc").fadeIn (600);
 			$("#tn1").text("Purifying Brew");
 			$("#tc1").text("Lorem ipsum dolor sit amet, onsectetur adipiscing elit. " +
@@ -131,7 +119,6 @@ $(document).ready(function () {
 			$(".row1-tc").css("display", "none");
 			$("#tn1").text("none")
 		} else {
-			$(".row2-tc").css("display", "none");
 			$(".row1-tc").fadeIn (600);
 			$("#tn1").text("Shuffle");
 			$("#tc1").text("Lorem ipsum dolor sit amet, onsectetur adipiscing elit. " +
@@ -147,7 +134,6 @@ $(document).ready(function () {
 			$(".row1-tc").css("display", "none");
 			$("#tn1").text("none")
 		} else {
-			$(".row2-tc").css("display", "none");
 			$(".row1-tc").fadeIn (600);
 			$("#tn1").text("Hit Scheme");
 			$("#tc1").text("Lorem ipsum dolor sit amet, onsectetur adipiscing elit. " +
@@ -163,7 +149,6 @@ $(document).ready(function () {
 			$(".row1-tc").css("display", "none");
 			$("#tn1").text("none")
 		} else {
-			$(".row2-tc").css("display", "none");
 			$(".row1-tc").fadeIn (600);
 			$("#tn1").text("gift of the ox");
 			$("#tc1").text("Lorem ipsum dolor sit amet, onsectetur adipiscing elit. " +
@@ -179,7 +164,6 @@ $(document).ready(function () {
 			$(".row1-tc").css("display", "none");
 			$("#tn1").text("none")
 		} else {
-			$(".row2-tc").css("display", "none");
 			$(".row1-tc").fadeIn (600);
 			$("#tn1").text("Healing elixir");
 			$("#tc1").text("Lorem ipsum dolor sit amet, onsectetur adipiscing elit. " +
@@ -195,7 +179,6 @@ $(document).ready(function () {
 			$(".row1-tc").css("display", "none");
 			$("#tn1").text("none")
 		} else {
-			$(".row2-tc").css("display", "none");
 			$(".row1-tc").fadeIn (600);
 			$("#tn1").text("Quick Sip");
 			$("#tc1").text("Lorem ipsum dolor sit amet, onsectetur adipiscing elit. " +
@@ -211,7 +194,6 @@ $(document).ready(function () {
 			$(".row1-tc").css("display", "none");
 			$("#tn1").text("none")
 		} else {
-			$(".row2-tc").css("display", "none");
 			$(".row1-tc").fadeIn (600);
 			$("#tn1").text("Rushing Jade Wind");
 			$("#tc1").text("Lorem ipsum dolor sit amet, onsectetur adipiscing elit. " +
@@ -227,7 +209,6 @@ $(document).ready(function () {
 			$(".row1-tc").css("display", "none");
 			$("#tn1").text("none")
 		} else {
-			$(".row2-tc").css("display", "none");
 			$(".row1-tc").fadeIn (600);
 			$("#tn1").text("Special Delivery");
 			$("#tc1").text("Lorem ipsum dolor sit amet, onsectetur adipiscing elit. " +
@@ -235,43 +216,209 @@ $(document).ready(function () {
 				"Praesent a maximus tortor, congue commodo turpis. Sed pharetra.");
 		}
 	});
-// 	ТАЛАНТЫ ХМЕЛЕВАРА 1-4
+// 	ТАЛАНТЫ ХМЕЛЕВАРА 1-5
 
-// 	ТАЛАНТЫ ХМЕЛЕВАРА 5-7
+// 	ТАЛАНТЫ ХМЕЛЕВАРА 6-10
 	$("#tt11").click(function () {
 		let name1 = "celestial flames"
-		let name2 = $("#tn2").text()
+		let name2 = $("#tn1").text()
 		if (name1 === name2) {
-			$(".row2-tc").css("display", "none");
-			$("#tn2").text("none")
-		} else {
 			$(".row1-tc").css("display", "none");
-			$(".row2-tc").fadeIn (600);
-			$("#tn2").text("celestial flames");
-			$("#tc2").text("Lorem ipsum dolor sit amet, onsectetur adipiscing elit. " +
+			$("#tn1").text("none")
+		} else {
+			$(".row1-tc").fadeIn (600);
+			$("#tn1").text("celestial flames");
+			$("#tc1").text("Lorem ipsum dolor sit amet, onsectetur adipiscing elit. " +
 				"Cras consequat volutpat viverra. Etiam fringilla quam nunc, quis dapibus sem accumsan nec. " +
 				"Praesent a maximus tortor, congue commodo turpis. Sed pharetra.");
 		}
 	});
-// 	ТАЛАНТЫ ХМЕЛЕВАРА 5-7
 
-// 	ТАЛАНТЫ ХМЕЛЕВАРА 8-10
+
+
 	$("#tt49").click(function () {
 		let name1 = "stormstout's last keg"
+		let name2 = $("#tn1").text()
+		if (name1 === name2) {
+			$(".row1-tc").css("display", "none");
+			$("#tn1").text("none")
+		} else {
+			$(".row1-tc").fadeIn (600);
+			$("#tn1").text("stormstout's last keg");
+			$("#tc1").text("Lorem ipsum dolor sit amet, onsectetur adipiscing elit. " +
+				"Cras consequat volutpat viverra. Etiam fringilla quam nunc, quis dapibus sem accumsan nec. " +
+				"Praesent a maximus tortor, congue commodo turpis. Sed pharetra.");
+		}
+	});
+// 	ТАЛАНТЫ ХМЕЛЕВАРА 6-10
+
+	// 	ТАЛАНТЫ МОНАХА 1-5
+	$("#ttm1").click(function () {
+		let name1 = "Успокаивающий туман"
 		let name2 = $("#tn2").text()
 		if (name1 === name2) {
 			$(".row2-tc").css("display", "none");
 			$("#tn2").text("none")
 		} else {
-			$(".row1-tc").css("display", "none");
 			$(".row2-tc").fadeIn (600);
-			$("#tn2").text("stormstout's last keg");
+			$("#tn2").text("Успокаивающий туман");
 			$("#tc2").text("Lorem ipsum dolor sit amet, onsectetur adipiscing elit. " +
 				"Cras consequat volutpat viverra. Etiam fringilla quam nunc, quis dapibus sem accumsan nec. " +
 				"Praesent a maximus tortor, congue commodo turpis. Sed pharetra.");
 		}
 	});
-// 	ТАЛАНТЫ ХМЕЛЕВАРА 8-10
+
+	$("#ttm2").click(function () {
+		let name1 = "удар восходящего солнца"
+		let name2 = $("#tn2").text()
+		if (name1 === name2) {
+			$(".row2-tc").css("display", "none");
+			$("#tn2").text("none")
+		} else {
+			$(".row2-tc").fadeIn (600);
+			$("#tn2").text("удар восходящего солнца");
+			$("#tc2").text("Lorem ipsum dolor sit amet, onsectetur adipiscing elit. " +
+				"Cras consequat volutpat viverra. Etiam fringilla quam nunc, quis dapibus sem accumsan nec. " +
+				"Praesent a maximus tortor, congue commodo turpis. Sed pharetra.");
+		}
+	});
+
+	$("#ttm3").click(function () {
+		let name1 = "Тигриное рвение"
+		let name2 = $("#tn2").text()
+		if (name1 === name2) {
+			$(".row2-tc").css("display", "none");
+			$("#tn2").text("none")
+		} else {
+			$(".row2-tc").fadeIn (600);
+			$("#tn2").text("Тигриное рвение");
+			$("#tc2").text("Lorem ipsum dolor sit amet, onsectetur adipiscing elit. " +
+				"Cras consequat volutpat viverra. Etiam fringilla quam nunc, quis dapibus sem accumsan nec. " +
+				"Praesent a maximus tortor, congue commodo turpis. Sed pharetra.");
+		}
+	});
+
+	$("#ttm4").click(function () {
+		let name1 = "улучшенный кувырок"
+		let name2 = $("#tn2").text()
+		if (name1 === name2) {
+			$(".row2-tc").css("display", "none");
+			$("#tn2").text("none")
+		} else {
+			$(".row2-tc").fadeIn (600);
+			$("#tn2").text("улучшенный кувырок");
+			$("#tc2").text("Lorem ipsum dolor sit amet, onsectetur adipiscing elit. " +
+				"Cras consequat volutpat viverra. Etiam fringilla quam nunc, quis dapibus sem accumsan nec. " +
+				"Praesent a maximus tortor, congue commodo turpis. Sed pharetra.");
+		}
+	});
+
+	$("#ttm5").click(function () {
+		let name1 = "Успокаивающее присутствие"
+		let name2 = $("#tn2").text()
+		if (name1 === name2) {
+			$(".row2-tc").css("display", "none");
+			$("#tn2").text("none")
+		} else {
+			$(".row2-tc").fadeIn (600);
+			$("#tn2").text("Успокаивающее присутствие");
+			$("#tc2").text("Lorem ipsum dolor sit amet, onsectetur adipiscing elit. " +
+				"Cras consequat volutpat viverra. Etiam fringilla quam nunc, quis dapibus sem accumsan nec. " +
+				"Praesent a maximus tortor, congue commodo turpis. Sed pharetra.");
+		}
+	});
+
+	$("#ttm6").click(function () {
+		let name1 = "Вывести из строя"
+		let name2 = $("#tn2").text()
+		if (name1 === name2) {
+			$(".row2-tc").css("display", "none");
+			$("#tn2").text("none")
+		} else {
+			$(".row2-tc").fadeIn (600);
+			$("#tn2").text("Вывести из строя");
+			$("#tc2").text("Lorem ipsum dolor sit amet, onsectetur adipiscing elit. " +
+				"Cras consequat volutpat viverra. Etiam fringilla quam nunc, quis dapibus sem accumsan nec. " +
+				"Praesent a maximus tortor, congue commodo turpis. Sed pharetra.");
+		}
+	});
+
+	$("#ttm7").click(function () {
+		let name1 = "взмах тигриного хвоста"
+		let name2 = $("#tn2").text()
+		if (name1 === name2) {
+			$(".row2-tc").css("display", "none");
+			$("#tn2").text("none")
+		} else {
+			$(".row2-tc").fadeIn (600);
+			$("#tn2").text("взмах тигриного хвоста");
+			$("#tc2").text("Lorem ipsum dolor sit amet, onsectetur adipiscing elit. " +
+				"Cras consequat volutpat viverra. Etiam fringilla quam nunc, quis dapibus sem accumsan nec. " +
+				"Praesent a maximus tortor, congue commodo turpis. Sed pharetra.");
+		}
+	});
+
+	$("#ttm8").click(function () {
+		let name1 = "энергичное исторжение"
+		let name2 = $("#tn2").text()
+		if (name1 === name2) {
+			$(".row2-tc").css("display", "none");
+			$("#tn2").text("none")
+		} else {
+			$(".row2-tc").fadeIn (600);
+			$("#tn2").text("энергичное исторжение");
+			$("#tc2").text("Lorem ipsum dolor sit amet, onsectetur adipiscing elit. " +
+				"Cras consequat volutpat viverra. Etiam fringilla quam nunc, quis dapibus sem accumsan nec. " +
+				"Praesent a maximus tortor, congue commodo turpis. Sed pharetra.");
+		}
+	});
+
+	$("#ttm9").click(function () {
+		let name1 = "Улучшенное 'Оживить'"
+		let name2 = $("#tn2").text()
+		if (name1 === name2) {
+			$(".row2-tc").css("display", "none");
+			$("#tn2").text("none")
+		} else {
+			$(".row2-tc").fadeIn (600);
+			$("#tn2").text("Улучшенное 'Оживить'");
+			$("#tc2").text("Lorem ipsum dolor sit amet, onsectetur adipiscing elit. " +
+				"Cras consequat volutpat viverra. Etiam fringilla quam nunc, quis dapibus sem accumsan nec. " +
+				"Praesent a maximus tortor, congue commodo turpis. Sed pharetra.");
+		}
+	});
+
+	$("#ttm10").click(function () {
+		let name1 = "Детоксикация"
+		let name2 = $("#tn2").text()
+		if (name1 === name2) {
+			$(".row2-tc").css("display", "none");
+			$("#tn2").text("none")
+		} else {
+			$(".row2-tc").fadeIn (600);
+			$("#tn2").text("Детоксикация");
+			$("#tc2").text("Lorem ipsum dolor sit amet, onsectetur adipiscing elit. " +
+				"Cras consequat volutpat viverra. Etiam fringilla quam nunc, quis dapibus sem accumsan nec. " +
+				"Praesent a maximus tortor, congue commodo turpis. Sed pharetra.");
+		}
+	});
+
+	$("#ttm11").click(function () {
+		let name1 = "Паралич"
+		let name2 = $("#tn2").text()
+		if (name1 === name2) {
+			$(".row2-tc").css("display", "none");
+			$("#tn2").text("none")
+		} else {
+			$(".row2-tc").fadeIn (600);
+			$("#tn2").text("Паралич");
+			$("#tc2").text("Lorem ipsum dolor sit amet, onsectetur adipiscing elit. " +
+				"Cras consequat volutpat viverra. Etiam fringilla quam nunc, quis dapibus sem accumsan nec. " +
+				"Praesent a maximus tortor, congue commodo turpis. Sed pharetra.");
+		}
+	});
+// 	ТАЛАНТЫ МОНАХА 1-5
+	
 // ТАЛАНТЫ
 
 });
